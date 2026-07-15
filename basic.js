5b135d60d8f1a84a93ea04b23ccc7b8429d4461c
sprogram24-dotcom <sprogram24@gmail.com>
2026-07-15 12:05:21 +0530

    firstJavaScript

---
+++ basic.js
@@ -0,0 +1,53 @@
+// console.log("this is sayan, learning JavaScript");
+// name= "sayan";
+// x=null;
+// y= undefined
+// console.log(x);
+// console.log(y);
+
+// var: can be re-declared and updated
+// var y=23
+// var y="xyz"
+// var y=55.5
+// console.log(y)
+
+// let: can not be re-drclared but can be updated
+// let age=24
+// age=34
+// console.log(age)
+
+// conts(constant): can not re-declared and updated
+// const pi=3.14
+// pi=22/7;  #error!
+// console.log(pi)
+
+// let name= "sayan"
+// let age=25
+// console.log(name)
+// console.log(age)
+
+// let a; # can be declared without initialization
+// const x=10 #const must be initialized else ERROR!
+
+// let age=25
+// let price = 100.00
+// console.log(typeof price);
+
+// const student={
+//     name:"sayan",
+//     Age:24,
+//     cgpa:8.9
+// }
+
+// student.name="SayanKumarPal"
+// console.log(student.name);
+
+// const profile={
+//     name:"sayan",
+//     followers:57000,
+//     instaHandle: "@sayanKrPal",
+//     No_post:200,
+//     description: "entreprenure"
+// }
+
+// console.log(profile)
